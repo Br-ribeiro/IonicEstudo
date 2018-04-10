22b3bcfdb2af3b3b8e9074c1a2227f2e9f928955
Br-ribeiro <bernardosr25@gmail.com>
2018-04-10 19:07:05 -0300

    criado o slide e o primeiro card

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
+import { CardsPage } from '../cards/cards';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,26 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+   slides = [
+     {
+       titulo: "testando",
+       descricao: "to testando mesmo se errar foda-se",
+       imagem: "assets/imgs/Johnny_large.png",
+     },
+     {
+      titulo: "test 2",
+      descricao: "to aprendendo porra",
+      imagem: "assets/imgs/briga.jpg",
+    },
+    {
+      titulo: "teste 3",
+      descricao: "GG agora foi",
+      imagem: "assets/imgs/comemorar.jpg",
+    }
+   ];
+
+   goPage(){
+     this.navCtrl.setRoot(CardsPage);
+   }
 
 }
